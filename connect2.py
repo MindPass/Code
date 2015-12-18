f2cb9842bb3e8f43165b53e8a7e8978740cffbb7
Martial <martial.garchery@minesdedouai.fr>
2015-12-18 07:54:31 +0100

    Changements inutiles

--- connect2.py
+++ connect2.py
@@ -6,11 +6,7 @@ from oauth2 import *
 client_id="287712233618-7qua8pervof64n6g740gi8d0o7ifiu28.apps.googleusercontent.com"
 client_secret=raw_input('Rentrer le client secret: ')
 
-<<<<<<< HEAD
 auth_code=raw_input("Auth code: ")
-=======
-auth_code="4/W1_AnPo2m1LygjzOao1faUFDJUYgemMmslfaMvftavI" #modifie par w précaution
->>>>>>> 597466c7ae5b0cec1d4a3653dff1eaef6b0cf2b8
 
 tokens= open("tokens.txt", "a")
  
