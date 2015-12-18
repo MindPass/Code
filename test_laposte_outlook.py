7d2f902e0b773fba070f4f07dd6442fd7000b3b4
Alexandre Domain <domain.alexandre@gmail.com>
2015-12-18 01:19:27 +0100

    Compatibilité Python 3.x + modifications mineures

--- test_laposte_outlook.py
+++ test_laposte_outlook.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Dec 03 23:00:10 2015
-
-@author: famille
-"""
-
-# -*-coding:utf-8 -*
+import imaplib
 
 user=input('entrez le pseudo: ')+"@outlook.com"
 mdp=input('Entrez le mdp: ')
@@ -21,12 +15,13 @@ id_list = ids.split() # ids is a space separated string
 latest_email_id = id_list[-1] # get the latest
 result, data = imap_conn.fetch(latest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID
 
-raw_email = data[0][1] # here's the body, which is raw text of the whole email
-# including headers and alternate payloads
-
-laposte= open("laposte.txt", "a")
-laposte.write(raw_email)
-laposte.close()
+# .decode('utf-8') for python 3.x compatibility (bytes -> str)
+raw_email = data[0][1].decode('utf-8') # here's the body, which is raw text of the whole email including headers and alternate payloads
 
 
-print raw_email
+email = open("laposte.txt", "w")
+email.write(raw_email)
+email.close()
+
+
+print(raw_email)
