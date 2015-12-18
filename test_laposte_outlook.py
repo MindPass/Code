295dbf27ebdf64aaadce318b89435eb7ec7e678b
Alexandre Domain <domain.alexandre@gmail.com>
2015-12-18 10:38:13 +0100

    ﻿Getpass / Prise en charge laposte.net / Var explicites / Màj mineures

--- test_laposte_outlook.py
+++ test_laposte_outlook.py
@@ -1,27 +1,33 @@
 # -*- coding: utf-8 -*-
-import imaplib
+import imaplib, getpass
 
 user=input('entrez le pseudo: ')+"@outlook.com"
-mdp=input('Entrez le mdp: ')
+#user=input('entrez le pseudo: ')+"@laposte.net"
+mdp=input('Entrez le mdp: ') # à garder pendant le développement
+#mdp=getpass.getpass('Entrez le mdp: ') # pour cacher le mdp lors de la saisie
 
 imap_conn = imaplib.IMAP4_SSL('imap-mail.outlook.com')
+#imap_conn = imaplib.IMAP4_SSL('imap.laposte.net')
 imap_conn.debug = 4
 imap_conn.login(user,mdp)
-imap_conn.select('INBOX')
+dansInbox=imap_conn.select('INBOX')
 
 result, data = imap_conn.search(None, "ALL")
 ids = data[0] # data is a list.
-id_list = ids.split() # ids is a space separated string
-latest_email_id = id_list[-1] # get the latest
+listeMailsInbox = ids.split() # ids is a space separated string
+latest_email_id = listeMailsInbox[-1] # get the latest
 result, data = imap_conn.fetch(latest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID
 
-# .decode('utf-8') for python 3.x compatibility (bytes -> str)
-raw_email = data[0][1].decode('utf-8') # here's the body, which is raw text of the whole email including headers and alternate payloads
+nombreMailsInbox = len(listeMailsInbox)
 
+raw_email = data[0][1].decode('utf-8') # .decode('utf-8') for python 3.x compatibility (bytes -> str)
 
-email = open("laposte.txt", "w")
+imap_conn.close()
+imap_conn.logout()
+
+email = open("outlook.txt", "w")
+#email = open("laposte.txt", "w")
 email.write(raw_email)
 email.close()
 
-
-print(raw_email)
+print( "Le nombre de messages est : " + str(nombreMailsInbox) )
