49776c2d67ef3d0b71d9bf6a948f6277b66a5921
Alexandre Domain <domain.alexandre@gmail.com>
2016-01-11 09:34:21 +0100

    Mise en commentaire + renommage maBaseDeDonnees

--- Etape 3/bdd.py
+++ Etape 3/bdd.py
@@ -1,29 +1,36 @@
 import sqlite3
 
-fichierDonnees = "mabasededonnées.sq3"
+fichierDonnees = "maBaseDeDonnees.sq3"
 
 conn = sqlite3.connect(fichierDonnees)
-
 cur = conn.cursor()
 
+# SUPPRIMER ET RECREEER LA BDD
+"""
 cur.execute ("DROP TABLE mails")
 cur.execute ("CREATE TABLE mails (expéditeur TEXT, destinataire TEXT, objet TEXT, contenu TEXT, date TEXT)")
+"""
 
 # INSERER UNE NOUVELLE LIGNE
+"""
 cur.execute ("INSERT INTO mails (expéditeur,destinataire,objet, contenu, date) VALUES('machintttt@lequipe.fr','FranckLopez@lol.fr', 'blabla', 'loret ipsum', '01/01/2016') ")
+"""
 
+# COMMIT
 conn.commit()
 
-
+# TRANSFORMATION EN TABLEAU
+"""
 cur.execute("SELECT * FROM mails")
-print(cur.fetchall()[3])
+print(cur.fetchall())
+"""
 
 cur.close()
 conn.close()
 
-# Pour se reconnecter à la base ensuite
+# POUR SE RECONNECTER PLUS TARD A LA BDD EXISTANTE
 """
 import sqlite3
-conn = sqlite3.connect("E:/python3/essais/bd_test.sq3") 
+conn = sqlite3.connect("maBaseDeDonnees.sq3") 
 cur = conn.cursor()
 """
