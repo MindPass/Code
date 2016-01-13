b74a62f36934277cded710fc4498331874e4c738
Alexandre Domain <domain.alexandre@gmail.com>
2016-01-13 22:27:07 +0100

    Plusieurs lignes + Alternance bg + Logo

--- Interface graphique/IF Remplissage ID-MDP.py
+++ Interface graphique/IF Remplissage ID-MDP.py
@@ -1,48 +1,70 @@
 from tkinter import *
 
+
+liste_test = ['facebook.com', 'twitter.com', 'lequipe.fr', 'eurosport.com', 'jaiplusdidee.fr']
+
+
 # Disparition du texte dans les entrées quand clic il y a
-firstclick = True
-
-def on_entry_click(event):
+firstclickID, firstclickMDP = True, True
+def on_entry_click_ID(event):
     """function that gets called whenever entry1 is clicked"""        
-    global firstclick
-
-    if firstclick: # if this is the first time they clicked it
-        firstclick = False
+    global firstclickID
+    if firstclickID: # if this is the first time they clicked it
+        firstclickID = False
         ID.delete(0, "end") # delete all the text in the entry
+def on_entry_click_MDP(event):
+    """function that gets called whenever entry1 is clicked"""        
+    global firstclickMDP
+    if firstclickMDP: # if this is the first time they clicked it
+        firstclickMDP = False
         MDP.delete(0, "end") # delete all the text in the entry
-
 
 # Création de la fenêtre principale
 Mafenetre = Tk()
-Mafenetre.title('Bienvenue sur MindPass :)')
+Mafenetre.title('Bienvenue sur MindPass')
 Mafenetre['bg']='lightblue' # couleur de fond
 
-frameConteneur = Frame(Mafenetre,borderwidth=0)
-frameConteneur.pack(side=LEFT,padx=10,pady=5,fill=X)
+photo = PhotoImage(file="LogoMindPass.png")
 
-frameOublier = Frame(frameConteneur)
-frameOublier.pack(side=LEFT,padx=10,pady=5)
-frameAdresse = Frame(frameConteneur,bg="red",borderwidth=2,relief=GROOVE)
-frameAdresse.pack(side=LEFT,padx=10,pady=5)
-frameID = Frame(frameConteneur,bg="blue",borderwidth=2,relief=GROOVE)
-frameID.pack(side=LEFT,padx=10,pady=5)
-frameMDP = Frame(frameConteneur,bg="green",borderwidth=2,relief=GROOVE)
-frameMDP.pack(side=LEFT,padx=10,pady=5)
+canvas = Canvas(Mafenetre,width=3000, height=445, bg="lightblue")
+canvas.create_image(0, 0, anchor=NW, image=photo)
+canvas.pack()
 
-Button(frameOublier,text="X",fg='navy',command=frameConteneur.destroy).pack(padx=10,pady=5,side=LEFT)
-Label(frameAdresse,text="facebook.com", fg="black", bg="red").pack(padx=10,side=LEFT)
 
-ID=Entry(frameID,text="Identifiant", fg="lightgrey", bg="blue")
+# non fonctionnelle pour l'instant
+scrollbar = Scrollbar(Mafenetre)
+scrollbar.pack(side = RIGHT, fill=Y )
+
+for i in range(len(liste_test)):
+    if i%2==0:
+        frameConteneur = Frame(Mafenetre,bg="#eaeaea",borderwidth=0)
+    else:
+        frameConteneur = Frame(Mafenetre,bg="#f6f6f6",borderwidth=0)
+    frameConteneur.pack(side=TOP,fill=X)
+    frameOublier = Frame(frameConteneur)
+    frameOublier.pack(side=LEFT)
+    frameAdresse = Frame(frameConteneur,bg="white",borderwidth=0,relief=FLAT)
+    frameAdresse.pack(side=LEFT,padx=20,pady=5)
+    frameID = Frame(frameConteneur,bg="white",borderwidth=2,relief=FLAT)
+    frameID.pack(side=LEFT,padx=20,pady=5)
+    frameMDP = Frame(frameConteneur,bg="white",borderwidth=2,relief=FLAT)
+    frameMDP.pack(side=LEFT,padx=20,pady=5)
+
+    Button(frameOublier,text="X",fg='navy',command=frameConteneur.destroy).pack(padx=10,pady=5,side=LEFT)
+    
+    Label(frameAdresse,text="www.{}".format(liste_test[i]), fg="black", width=30).pack(side=LEFT)
+    
+    ID=Entry(frameID,text="Identifiant", fg="#bbb", bg="white", borderwidth=0)
+    ID.bind('<FocusIn>', on_entry_click_ID)
+    ID.pack(padx=10,side=LEFT)
+    
+    MDP=Entry(frameMDP,text="Mot de Passe", fg="#bbb", bg="white", borderwidth=0) # ajouter <<<show="*">>> pour ne pas voir les caractères saisis
+    MDP.bind('<FocusIn>', on_entry_click_MDP)
+    MDP.pack(padx=10,side=LEFT)
+
 ID.insert(0, "Identifiant")
-ID.bind('<FocusIn>', on_entry_click)
-ID.pack(padx=10,side=LEFT)
-
-MDP=Entry(frameMDP,text="Mot de Passe", fg="lightgrey", bg="green", show="*")
 MDP.insert(0, "Mot de passe")
-MDP.bind('<FocusIn>', on_entry_click)
-MDP.pack(padx=10,side=LEFT)
 
-Mafenetre.state('zoomed') # Page plein écran
+Mafenetre.state('zoomed') # Page affichée en plein écran
 
 Mafenetre.mainloop()
