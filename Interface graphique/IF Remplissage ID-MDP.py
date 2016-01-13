cf6c5da2320b0a0771e8a97cba1fe58a414635dc
Alexandre Domain <domain.alexandre@gmail.com>
2016-01-13 17:48:21 +0100

    Amélioration IF : ajout complétion

--- Interface graphique/IF Remplissage ID-MDP.py
+++ Interface graphique/IF Remplissage ID-MDP.py
@@ -1,19 +1,47 @@
 from tkinter import *
- 
+
+# Disparition du texte dans les entrées quand clic il y a
+firstclick = True
+
+def on_entry_click(event):
+    """function that gets called whenever entry1 is clicked"""        
+    global firstclick
+
+    if firstclick: # if this is the first time they clicked it
+        firstclick = False
+        ID.delete(0, "end") # delete all the text in the entry
+        MDP.delete(0, "end") # delete all the text in the entry
+
 
 # Création de la fenêtre principale
 Mafenetre = Tk()
-Mafenetre.title('Bienvenue sur MindPass')
+Mafenetre.title('Bienvenue sur MindPass :)')
 Mafenetre['bg']='lightblue' # couleur de fond
 
-Frame1 = Frame(Mafenetre,borderwidth=2,relief=GROOVE)
-Frame1.pack(side=LEFT,padx=10,pady=5)
+frameConteneur = Frame(Mafenetre,borderwidth=0)
+frameConteneur.pack(side=LEFT,padx=10,pady=5,fill=X)
 
-Button(Frame1,text="X",fg='navy',command=Frame1.destroy).pack(padx=10,pady=5,side=LEFT)
-Label(Frame1,text="facebook.com", fg="black").pack(padx=10,side=LEFT)
-Label(Frame1,text="Identifiant", fg="lightgrey").pack(padx=10,side=LEFT)
-Label(Frame1,text="Mot de Passe", fg="lightgrey").pack(padx=10,side=LEFT)
-#Button(Frame1,text="Effacer",fg='navy',command=Frame1.destroy).pack(padx=10,pady=10)
+frameOublier = Frame(frameConteneur)
+frameOublier.pack(side=LEFT,padx=10,pady=5)
+frameAdresse = Frame(frameConteneur,bg="red",borderwidth=2,relief=GROOVE)
+frameAdresse.pack(side=LEFT,padx=10,pady=5)
+frameID = Frame(frameConteneur,bg="blue",borderwidth=2,relief=GROOVE)
+frameID.pack(side=LEFT,padx=10,pady=5)
+frameMDP = Frame(frameConteneur,bg="green",borderwidth=2,relief=GROOVE)
+frameMDP.pack(side=LEFT,padx=10,pady=5)
+
+Button(frameOublier,text="X",fg='navy',command=frameConteneur.destroy).pack(padx=10,pady=5,side=LEFT)
+Label(frameAdresse,text="facebook.com", fg="black", bg="red").pack(padx=10,side=LEFT)
+
+ID=Entry(frameID,text="Identifiant", fg="lightgrey", bg="blue")
+ID.insert(0, "Identifiant")
+ID.bind('<FocusIn>', on_entry_click)
+ID.pack(padx=10,side=LEFT)
+
+MDP=Entry(frameMDP,text="Mot de Passe", fg="lightgrey", bg="green", show="*")
+MDP.insert(0, "Mot de passe")
+MDP.bind('<FocusIn>', on_entry_click)
+MDP.pack(padx=10,side=LEFT)
 
 Mafenetre.state('zoomed') # Page plein écran
 
