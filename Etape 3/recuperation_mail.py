103b7aa62c13c6c26dc48aeb7a357f63335dca90
Alexandre Domain <domain.alexandre@gmail.com>
2016-01-14 14:03:53 +0100

    Simplification du code

--- Etape 3/recuperation_mail.py
+++ Etape 3/recuperation_mail.py
@@ -1,12 +1,10 @@
 # coding: utf8
 import imaplib, urllib.parse, re
-from email.parser import Parser
-from email.parser import FeedParser
+from email.parser import Parser, FeedParser
 
 #user=input('entrez le pseudo: ')+"@outlook.com"
 user=input('entrez le pseudo: ')+"@laposte.net"
 mdp=input('Entrez le mdp: ') # à garder pendant le développement
-
 
 #imap_conn = imaplib.IMAP4_SSL('imap-mail.outlook.com')
 imap_conn = imaplib.IMAP4_SSL('imap.laposte.net')
@@ -18,7 +16,7 @@ result, data = imap_conn.search(None, "ALL")
 ids = data[0] # data is a list.
 listeMailsInbox = ids.split() # ids is a space separated string
 
-latest_email_id = listeMailsInbox[-1] # get the latest
+latest_email_id = listeMailsInbox[-2] # get the latest
 result, data = imap_conn.fetch(latest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID
 
 nombreMailsInbox = len(listeMailsInbox)
@@ -26,6 +24,7 @@ nombreMailsInbox = len(listeMailsInbox)
 
 raw_email = data[0][1].decode('utf-8')
 # .decode('utf-8') for python 3.x compatibility (bytes -> str)
+mail = Parser().parsestr(raw_email)
 
 imap_conn.close()
 imap_conn.logout()
@@ -38,28 +37,30 @@ rootMessage = f.close()
 corps=rootMessage.get_payload(0).get_payload(decode=True).decode('utf-8')
 # Récupérer le corps du mail en plain/text bien décodé
 
+""" si on veut tester dans un txt
 open("laposte.txt","w").close()
 email = open("laposte.txt", "w")
 email.write(raw_email)
 email.close()
-
-print(mail['From'])
+"""
 
 """ méthode Alex """
-subject=mail['Subject']
+# suppression des entêtes de merde avec une regexp
+subject=rootMessage.get('Subject')
 for i in range(len(subject)):
     if subject[i] == "=":
         subject = subject[:i] + "%" + subject[i+1:]
     elif subject[i] == "_":
-        subject = subject[:i] + " " + subject[i+1:]
-# suppression des entêtes de merde avec une regexp
+         subject = subject[:i] + " " + subject[i+1:]
 subject = re.sub('(\n)*\%\?(UTF|utf)\-8\?(Q|B|q|b)\? *', '', subject)
 subject = re.sub('\?\%(\r\n)*', '', subject)
 subject=urllib.parse.unquote(subject) 
-print(subject) # <<<mail['Subject']>>> modifié avec le bon décodage et sans superflu
-""" fin méthode Alex """
+"""fin méthode Alex """
 
-print(corps)
-print(mail['Date'])
+print (subject)
+print (rootMessage.get('From'))
+print (rootMessage.get('Date'))
+print (corps)
+
 
 #print( "Le nombre de messages est : " + str(nombreMailsInbox) )
