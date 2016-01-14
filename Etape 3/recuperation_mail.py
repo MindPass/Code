8fa5240b83c4889054100b22c65ca335c0b68cf2
Martial <martial.garchery@minesdedouai.fr>
2016-01-14 13:47:44 +0100

    Problème  encodage résolu

--- Etape 3/recuperation_mail.py
+++ Etape 3/recuperation_mail.py
@@ -1,6 +1,7 @@
 # coding: utf8
 import imaplib, urllib.parse, re
 from email.parser import Parser
+from email.parser import FeedParser
 
 #user=input('entrez le pseudo: ')+"@outlook.com"
 user=input('entrez le pseudo: ')+"@laposte.net"
@@ -16,28 +17,33 @@ dansInbox=imap_conn.select('INBOX')
 result, data = imap_conn.search(None, "ALL")
 ids = data[0] # data is a list.
 listeMailsInbox = ids.split() # ids is a space separated string
+
 latest_email_id = listeMailsInbox[-1] # get the latest
 result, data = imap_conn.fetch(latest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID
 
 nombreMailsInbox = len(listeMailsInbox)
 
+
 raw_email = data[0][1].decode('utf-8')
 # .decode('utf-8') for python 3.x compatibility (bytes -> str)
-mail = Parser().parsestr(raw_email)
 
 imap_conn.close()
 imap_conn.logout()
 
+#http://stackoverflow.com/questions/4040074/python-email-encoding-problem
+f = FeedParser()
+f.feed(raw_email)
+rootMessage = f.close()
+
+corps=rootMessage.get_payload(0).get_payload(decode=True).decode('utf-8')
+# Récupérer le corps du mail en plain/text bien décodé
 
 open("laposte.txt","w").close()
 email = open("laposte.txt", "w")
 email.write(raw_email)
 email.close()
 
-"""
-print(mail['To'])
 print(mail['From'])
-"""
 
 """ méthode Alex """
 subject=mail['Subject']
@@ -53,14 +59,7 @@ subject=urllib.parse.unquote(subject)
 print(subject) # <<<mail['Subject']>>> modifié avec le bon décodage et sans superflu
 """ fin méthode Alex """
 
-#print(mail['Content-Type'])
-#print(mail['Date'])
-
-
-"""
-print(mail['From'])
-print(mail['Subject'])
-"""
-
+print(corps)
+print(mail['Date'])
 
 #print( "Le nombre de messages est : " + str(nombreMailsInbox) )
