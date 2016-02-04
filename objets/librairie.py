687e52ccac6a143cc78e689262daf0b37f9fc0d1
Martial <martial.garchery@minesdedouai.fr>
2016-02-05 00:03:07 +0100

    Commentaires plus pertinents dans librairie.py

--- objets/librairie.py
+++ objets/librairie.py
@@ -3,26 +3,33 @@ import sqlite3
 
 class Table(object):
 	"""docstring for Table
-	Définit une table dans la bdd, destinée à contenir les mails d'une adresse données.
+	Définit une table dans la bdd, destinée à contenir les mails d'une adresse donnée; classe
+	utile si l'utilisateur nous en fournit plusieurs.
 
-	Attributs:
-	-name, est en fait l'adresse mail de l'utilisateur (str), utile dans le cas où
-	il nous fournit plusieurs adresses.
-	-bdd, fichier .sq3 qui contient les données.
-	Méthodes:
-	-la connexion se fait lors du constructeur.
+	Attributs,
+	De classe:
+	-bdd est le fichier .sq3 contenant les données.
+	D'instance:
+	-name, qui peut être l'adresse donnée.
+	-conn et cur, sont des variables permettant de se conecter à une bdd en sqlite (.sq3).
 	"""
+
 	bdd="MaBaseDeDonnes.sq3"
 	def __init__(self, name):
+		"""Constructeur
+		On créé la table name si elle n'existe pas
+		"""
 		self.name = name
 		self.conn=sqlite3.connect(Table.bdd)
 		self.cur=self.conn.cursor()
 		self.cur.execute ("CREATE TABLE IF NOT EXISTS "+self.name+\
 			" (id TEXT, expéditeur TEXT, sujet TEXT, contenu TEXT, date TEXT)")
+		#se prémunir d'une injection SQL reste à faire
 
 	def addMail(self, id_message, expediteur, sujet, contenu, date):
 		self.cur.execute("INSERT INTO "+self.name+" (id, expéditeur, sujet, contenu, date) VALUES(?,?,?,?,?)",\
 			(id_message, expediteur, sujet, contenu, date))
+		#se prémunir d'une injection SQL reste à faire
 
 	def saveAndClose(self):
 		self.conn.commit()
