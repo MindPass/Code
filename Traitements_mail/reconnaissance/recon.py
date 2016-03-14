637597ce256d3a76d6e4ca82185f141ca15d7ce5
Alexandre Domain <domain.alexandre@gmail.com>
2016-03-14 23:28:37 +0100

    regex reconnaissance expéditeur + nom domaine

--- Traitements_mail/reconnaissance/recon.py
+++ Traitements_mail/reconnaissance/recon.py
@@ -17,16 +17,22 @@ def print_(arg):
 conn = sqlite3.connect('../objets/bdd.sq3')
 cur = conn.cursor()
 
-cur.execute("CREATE TABLE IF NOT EXISTS sites_reconnus (id INT, mail_adress TEXT, website TEXT)")
+cur.execute("CREATE TABLE IF NOT EXISTS sites_reconnus (mail_adress TEXT, website TEXT, PRIMARY KEY (mail_adress))")
 
 cur.execute("SELECT DISTINCT expediteur FROM mindpasstest_laposte")
 tableau = cur.fetchall()
 
 liste_exp = []
+liste_domaines = []
+regex_expediteur = "<?([A-Za-z0-9]+@(?:(?:[A-Za-z0-9])*\.)*([A-Za-z0-9]+\.[A-Za-z]{1,5}))>?"
+
 for champs in tableau:
-    liste_exp.append(champs[0])
+    expediteur = re.findall(regex_expediteur, champs[0])
+    liste_exp.append(expediteur[0][0])
+    liste_domaines.append("www." + expediteur[0][1])
 
 print_(liste_exp)
+print_(liste_domaines)
 
 conn.commit()
 cur.close()
