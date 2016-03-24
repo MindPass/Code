8a676b9d2374e791de467463f4e9c2676e22b837
Martial <martial.garchery@minesdedouai.fr>
2016-03-24 18:12:10 +0100

    Fenetre Resizable, element qui s'adapte et autres ajouts sympas

--- Interface_graphique/PyQt/application/classeGestion.py
+++ Interface_graphique/PyQt/application/classeGestion.py
@@ -1,7 +1,8 @@
 import sqlite3
 import sys
-from functools import partial
+
 sys.path.append('../fenetres/')
+from functools import partial
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore
 from fenetreGestion import Ui_fenetreGestion
@@ -21,15 +22,15 @@ def print_(arg):
     print("-------------------------------------")
 
 
-#Filtre les événements de FocusOut (ex pour une lineEdit)
+# Filtre les événements de FocusOut (ex pour une lineEdit)
 class Filter(QtCore.QObject):
     def eventFilter(self, widget, event):
         # FocusOut event
         if event.type() == QtCore.QEvent.FocusOut:
             # do custom stuff
-            print(event)
             print(widget)
             print(widget.text())
+            print("focus out")
             # return False so that the widget will also handle the event
             # otherwise it won't focus out
             return False
@@ -37,18 +38,26 @@ class Filter(QtCore.QObject):
             # we don't care about other events
             return False
 
+
 class ClasseGestion(Ui_fenetreGestion):
     def __init__(self, fenetre):
         self.setupUi(fenetre)
-        self.h_layouts = []
-        self.lignes_cat = []
-        self._filter = Filter() #filtre pour les lineEdit
-
-    def lancement(self):
-        self.afficher_sites()
-        self.afficher_categories()
         self.ajouter_cat.setPlaceholderText("Ajouter une catégorie")
         self.ajouter_cat.returnPressed.connect(self.check_if_exist)
+
+        # Alignement des lignes en haut
+        self.verticalLayout.setAlignment(QtCore.Qt.AlignTop)
+        self.verticalLayout_2.setAlignment(QtCore.Qt.AlignTop)
+        self.verticalLayout_3.setAlignment(QtCore.Qt.AlignTop)
+
+        self.lignes_site = []
+        self.lignes_cat = []
+        self._filter = Filter()
+
+    def lancement(self):
+        # A mettre dans lancement()
+        self.afficher_sites()
+        self.afficher_categories()
 
     def check_if_exist(self):
         """
@@ -70,6 +79,7 @@ class ClasseGestion(Ui_fenetreGestion):
                 self.ajouter_categorie()
 
     def afficher_categories(self):
+        self.label_cats = []
         conn = sqlite3.connect(bdd)
         cur = conn.cursor()
         cur.execute('SELECT nom_categorie FROM categories')
@@ -84,28 +94,24 @@ class ClasseGestion(Ui_fenetreGestion):
         conn.close()
 
     def ajouter_ligne_categorie(self, y, nom_categorie):
-        self.lignes_cat.append({"horizontalLayoutWidget_2": QtWidgets.QWidget(self.scrollAreaWidgetContents_cat)})
-        self.scrollAreaWidgetContents_cat.setMinimumSize(QtCore.QSize(0, (y + 1) * 40+10))
-        self.lignes_cat[y]["horizontalLayoutWidget_2"].setGeometry(QtCore.QRect(10, y*40+10, 201, 41))
-        self.lignes_cat[y]["horizontalLayoutWidget_2"].setObjectName("horizontalLayoutWidget_2")
-        self.lignes_cat[y]["ligne_cat"] = QtWidgets.QHBoxLayout(self.lignes_cat[y]["horizontalLayoutWidget_2"])
-        self.lignes_cat[y]["ligne_cat"].setObjectName("ligne_cat")
-        self.lignes_cat[y]["label_cat"] = QtWidgets.QLabel(self.lignes_cat[y]["horizontalLayoutWidget_2"])
-        self.lignes_cat[y]["label_cat"].setObjectName("label_cat")
-        self.lignes_cat[y]["label_cat"].setText(nom_categorie)
-        self.lignes_cat[y]["ligne_cat"].addWidget(self.lignes_cat[y]["label_cat"])
-        self.lignes_cat[y]["pushButton_cat"] = QtWidgets.QPushButton(self.lignes_cat[y]["horizontalLayoutWidget_2"])
-        self.lignes_cat[y]["pushButton_cat"].setEnabled(True)
-        self.lignes_cat[y]["pushButton_cat"].setObjectName("pushButton_cat")
-        self.lignes_cat[y]["pushButton_cat"].setText('X')
-        self.lignes_cat[y]["ligne_cat"].addWidget(self.lignes_cat[y]["pushButton_cat"])
-        self.lignes_cat[y]["ligne_cat"].setStretch(0, 9)
-        self.lignes_cat[y]["ligne_cat"].setStretch(1, 1)
+        self.lignes_cat.append({'ligne_categorie': QtWidgets.QHBoxLayout()})
+        self.lignes_cat[y]['ligne_categorie'].setObjectName("ligne_categorie")
+        self.lignes_cat[y]['label_cat'] = QtWidgets.QLabel(self.scrollAreaWidgetContents_cat)
+        self.lignes_cat[y]['label_cat'].setMaximumSize(QtCore.QSize(16777215, 608))
+        self.lignes_cat[y]['label_cat'].setObjectName("label_cat")
+        self.lignes_cat[y]['label_cat'].setText(nom_categorie)
+        self.lignes_cat[y]['ligne_categorie'].addWidget(self.lignes_cat[y]['label_cat'])
+        self.lignes_cat[y]['pushButton_cat'] = QtWidgets.QPushButton(self.scrollAreaWidgetContents_cat)
+        self.lignes_cat[y]['pushButton_cat'].setEnabled(True)
+        self.lignes_cat[y]['pushButton_cat'].setObjectName("pushButton_cat")
+        self.lignes_cat[y]['pushButton_cat'].setText('X')
+        self.lignes_cat[y]['ligne_categorie'].addWidget(self.lignes_cat[y]['pushButton_cat'])
+        self.verticalLayout_3.addLayout(self.lignes_cat[y]['ligne_categorie'])
 
-        # Affichage du layout
-        self.lignes_cat[y]["horizontalLayoutWidget_2"].show()
+        # On garde l'alignement haut
+        self.verticalLayout_3.setAlignment(QtCore.Qt.AlignTop)
 
-        # Appel de la fonction supprimer_cat de paramètre y
+        # Evenement quand le boutton de suppression est cliqué
         self.lignes_cat[y]["pushButton_cat"].clicked.connect(partial(self.supprimer_cat, y=y))
 
     def ajouter_categorie(self):
@@ -128,7 +134,7 @@ class ClasseGestion(Ui_fenetreGestion):
         cur.close()
         conn.close()
 
-        print_("Suppression de "+str(self.lignes_cat[y]["label_cat"].text()))
+        print("Suppression de " + str(self.lignes_cat[y]["label_cat"].text()))
 
         # destruction des layouts dans la scroll_area
         self.scrollAreaWidgetContents_cat.deleteLater()
@@ -136,8 +142,10 @@ class ClasseGestion(Ui_fenetreGestion):
         self.lignes_cat = []
         # On en recrée un vide
         self.scrollAreaWidgetContents_cat = QtWidgets.QWidget()
-        self.scrollAreaWidgetContents_cat.setGeometry(QtCore.QRect(0, 0, 219, 742))
+        self.scrollAreaWidgetContents_cat.setGeometry(QtCore.QRect(0, 0, 177, 767))
         self.scrollAreaWidgetContents_cat.setObjectName("scrollAreaWidgetContents_cat")
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents_cat)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
         self.scrollArea_cat.setWidget(self.scrollAreaWidgetContents_cat)
 
         self.afficher_categories()
@@ -149,67 +157,61 @@ class ClasseGestion(Ui_fenetreGestion):
         tab = cur.fetchall()
 
         for k in range(len(tab)):
-            self.ajouter_ligne(k, tab[k][0], tab[k][1], tab[k][2], tab[k][3])
+            self.ajouter_ligne_site(k, tab[k][0], tab[k][1], tab[k][2], tab[k][3])
 
         conn.commit()
         cur.close()
         conn.close()
 
-    def ajouter_ligne(self, y, site_web, identifiant, mdp, categorie):
-        self.h_layouts.append({"horizontalLayoutWidget": QtWidgets.QWidget(self.scrollAreaWidgetContents)})
-        self.h_layouts[y]["horizontalLayoutWidget"].setGeometry(QtCore.QRect(0, y * 80, 881, 80))
-        self.scrollAreaWidgetContents.setMinimumSize(QtCore.QSize(0, (y + 1) * 80))
-        self.h_layouts[y]["horizontalLayoutWidget"].setObjectName("horizontalLayoutWidget" + str(y))
-        self.h_layouts[y]["ligne"] = QtWidgets.QHBoxLayout(self.h_layouts[y]["horizontalLayoutWidget"])
-        self.h_layouts[y]["ligne"].setObjectName("ligne" + str(y))
-        self.h_layouts[y]["site_web"] = QtWidgets.QLabel(self.h_layouts[y]["horizontalLayoutWidget"])
-        self.h_layouts[y]["site_web"].setAlignment(QtCore.Qt.AlignCenter)
-        self.h_layouts[y]["site_web"].setText(site_web)
-        self.h_layouts[y]["site_web"].setObjectName("site_web" + str(y))
-        self.h_layouts[y]["ligne"].addWidget(self.h_layouts[y]["site_web"])
-        self.h_layouts[y]["identifiant"] = QtWidgets.QLineEdit(self.h_layouts[y]["horizontalLayoutWidget"])
-        self.h_layouts[y]["identifiant"].setAlignment(QtCore.Qt.AlignCenter)
+    def ajouter_ligne_site(self, y, site_web, identifiant, mdp, categorie):
+        self.lignes_site.append({'ligne_site': QtWidgets.QHBoxLayout()})
+        self.lignes_site[y]['ligne_site'] = QtWidgets.QHBoxLayout()
+        self.lignes_site[y]['ligne_site'].setObjectName("ligne_site")
+        self.lignes_site[y]['site_web'] = QtWidgets.QLabel(self.scrollAreaWidgetContents_sites)
+        self.lignes_site[y]['site_web'].setAlignment(QtCore.Qt.AlignCenter)
+        self.lignes_site[y]['site_web'].setObjectName("site_web")
+        self.lignes_site[y]['site_web'].setText(site_web)
+        self.lignes_site[y]['ligne_site'].addWidget(self.lignes_site[y]['site_web'])
+        self.lignes_site[y]['identifiant'] = QtWidgets.QLineEdit(self.scrollAreaWidgetContents_sites)
+        self.lignes_site[y]['identifiant'].setAlignment(QtCore.Qt.AlignCenter)
+        self.lignes_site[y]['identifiant'].setObjectName("identifiant")
 
         if identifiant is None:
-            self.h_layouts[y]["identifiant"].setPlaceholderText("Ajouter un pseudo")
+            self.lignes_site[y]['identifiant'].setPlaceholderText("Ajouter un pseudo")
         else:
-            self.h_layouts[y]["identifiant"].setText(identifiant)
+            self.lignes_site[y]['identifiant'].setText(identifiant)
 
-        self.h_layouts[y]["identifiant"].installEventFilter(self._filter)
-        self.h_layouts[y]["identifiant"].setObjectName("identifiant" + str(y))
-        self.h_layouts[y]["ligne"].addWidget(self.h_layouts[y]["identifiant"])
-        self.h_layouts[y]["mdp"] = QtWidgets.QLineEdit(self.h_layouts[y]["horizontalLayoutWidget"])
-        self.h_layouts[y]["mdp"].setAlignment(QtCore.Qt.AlignCenter)
-        self.h_layouts[y]["mdp"].setObjectName("mdp" + str(y))
+        # Event filter pour les identifiants
+        self.lignes_site[y]["identifiant"].installEventFilter(self._filter)
+
+        self.lignes_site[y]['ligne_site'].addWidget(self.lignes_site[y]['identifiant'])
+        self.lignes_site[y]['mdp'] = QtWidgets.QLineEdit(self.scrollAreaWidgetContents_sites)
+        self.lignes_site[y]['mdp'].setMaximumSize(QtCore.QSize(16777215, 40))
+        self.lignes_site[y]['mdp'].setObjectName("mdp")
 
         if mdp is None:
-            self.h_layouts[y]["mdp"].setPlaceholderText("Ajouter un mdp")
+            self.lignes_site[y]['mdp'].setPlaceholderText("Ajouter un mdp")
         else:
-            self.h_layouts[y]["mdp"].setText(mdp)
+            self.lignes_site[y]['mdp'].setText(mdp)
 
 
-        self.h_layouts[y]["ligne"].addWidget(self.h_layouts[y]["mdp"])
-        self.h_layouts[y]["categorie"] = QtWidgets.QCheckBox(self.h_layouts[y]["horizontalLayoutWidget"])
-        self.h_layouts[y]["categorie"].setText(categorie)
-        self.h_layouts[y]["categorie"].setObjectName("categorie" + str(y))
-        self.h_layouts[y]["ligne"].addWidget(self.h_layouts[y]["categorie"])
-        self.h_layouts[y]["ligne"].setStretch(0, 100)
-        self.h_layouts[y]["ligne"].setStretch(1, 120)
-        self.h_layouts[y]["ligne"].setStretch(2, 120)
-        self.h_layouts[y]["ligne"].setStretch(3, 10)
+        self.lignes_site[y]['ligne_site'].addWidget(self.lignes_site[y]['mdp'])
+        self.lignes_site[y]['categorie'] = QtWidgets.QCheckBox(self.scrollAreaWidgetContents_sites)
+        self.lignes_site[y]['categorie'].setEnabled(True)
+        self.lignes_site[y]['categorie'].setText(categorie)
+        self.lignes_site[y]['categorie'].setObjectName("categorie")
+        self.lignes_site[y]['ligne_site'].addWidget(self.lignes_site[y]['categorie'])
+        self.lignes_site[y]['ligne_site'].setStretch(0, 1)
+        self.lignes_site[y]['ligne_site'].setStretch(1, 1)
+        self.lignes_site[y]['ligne_site'].setStretch(2, 1)
+        self.verticalLayout.addLayout(self.lignes_site[y]['ligne_site'])
 
-        # On affiche le layout
-        self.h_layouts[y]["horizontalLayoutWidget"].show()
-
-    def modification_identifiant(self, y):
-        print_(self.h_layouts[y]["identifiant"].text())
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     fenetreGestion = QtWidgets.QMainWindow()
 
     classGestion = ClasseGestion(fenetreGestion)
-    classGestion.lancement()
 
     fenetreGestion.show()
     sys.exit(app.exec_())
